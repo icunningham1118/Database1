f2b9cbc1cea102958610d99914e7baa37f1cc1b9
Ian Cunningham <icunning1118@gmail.com>
2025-08-27 21:11:05 -0500

    Added json library for serialization. Removed some unnecessary lines from main. Included the library for serialization to main header.

--- Assignment1/main.h
+++ Assignment1/main.h
@@ -5,8 +5,11 @@
 #include <fstream>
 #include <string>
 #include <sstream>
+#include <queue>
 #include "item.h"
+#include "table.h"
 #include "functions.h"
 #include "data.h"
+#include "json.hpp"
 
 #endif /*MAIN_H*/
