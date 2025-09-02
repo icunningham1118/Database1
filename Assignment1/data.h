86978fadb0cb5a836959efc71e291921b9cda9e0
Ian Cunningham <icunning1118@gmail.com>
2025-09-01 22:47:42 -0500

    Added json constructor macro from json library to the Data struct.

--- Assignment1/data.h
+++ Assignment1/data.h
@@ -3,11 +3,18 @@
 #define DATA_H
 
 #include <string>
+#include "json.hpp"
 
-struct Data {
-	int key = 0, size = 0;
-	float price = 0.0;
-	std::string name, container, brand, mfgr, comment, type;
-	std::string line;
-};
+using json = nlohmann::json;
+namespace dj {
+	struct Data {
+		int key = 0, size = 0;
+		float price = 0.0;
+		std::string name, container, brand, mfgr, comment, type;
+		NLOHMANN_DEFINE_TYPE_INTRUSIVE(Data, key, name, mfgr, brand, type, size, container, price, comment);
+	};
+
+	//void to_json(json&, Data&);
+	//void from_json(const json&, Data&);
+} //namespace dj
 #endif //DATA_H
